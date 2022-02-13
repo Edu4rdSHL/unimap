a41b1807cbdf3323b1bfb98f3d00ee91ba9423eb
Edu4rdSHL <edu4rdshl@protonmail.com>
2022-02-13 06:08:27 -0500

    Update builder.sh Cargo.toml: add [profile.release]
    Signed-off-by: Edu4rdSHL <edu4rdshl@protonmail.com>

--- builder.sh
+++ builder.sh
@@ -3,6 +3,7 @@
 NAME="unimap"
 
 LINUX_TARGET="x86_64-unknown-linux-musl"
+LINUX_X86_TARGET="i686-unknown-linux-musl"
 WIN_TARGET="x86_64-pc-windows-gnu"
 ARMV7_TARGET="armv7-unknown-linux-gnueabihf"
 AARCH_TARGET="aarch64-unknown-linux-gnu"
@@ -14,8 +15,21 @@ echo "Building Linux artifact."
 if cargo build -q --release --target="$LINUX_TARGET"; then
   echo "Linux artifact build: SUCCESS"
   cp "target/$LINUX_TARGET/release/$NAME" "target/$LINUX_TARGET/release/$NAME-linux"
+  strip "target/$LINUX_TARGET/release/$NAME-linux"
+  sha512sum "target/$LINUX_TARGET/release/$NAME-linux" >"target/$LINUX_TARGET/release/$NAME-linux.sha512"
 else
   echo "Linux artifact build: FAILED"
+fi
+
+# Linux x86 build
+echo "Building Linux x86 artifact."
+if cross build -q --release --target="$LINUX_X86_TARGET"; then
+  echo "Linux x86 artifact build: SUCCESS"
+  cp "target/$LINUX_X86_TARGET/release/$NAME" "target/$LINUX_X86_TARGET/release/$NAME-linux-i386"
+  strip "target/$LINUX_X86_TARGET/release/$NAME-linux-i386"
+  sha512sum "target/$LINUX_X86_TARGET/release/$NAME-linux-i386" >"target/$LINUX_X86_TARGET/release/$NAME-linux-i386.sha512"
+else
+  echo "Linux x86 artifact build: FAILED"
 fi
 
 # Windows build
@@ -23,6 +37,8 @@ echo "Building Windows artifact."
 if cross build -q --release --target="$WIN_TARGET"; then
   echo "Windows artifact build: SUCCESS"
   cp "target/$WIN_TARGET/release/$NAME.exe" "target/$WIN_TARGET/release/$NAME-windows.exe"
+  strip "target/$WIN_TARGET/release/$NAME-windows.exe"
+  sha512sum "target/$WIN_TARGET/release/$NAME-windows.exe" >"target/$WIN_TARGET/release/$NAME-windows.exe.sha512"
 else
   echo "Windows artifact build: FAILED"
 fi
@@ -32,6 +48,8 @@ echo "Building ARMv7 artifact."
 if cross build -q --release --target="$ARMV7_TARGET"; then
   echo "ARMv7 artifact build: SUCCESS"
   cp "target/$ARMV7_TARGET/release/$NAME" "target/$ARMV7_TARGET/release/$NAME-armv7"
+  strip "target/$ARMV7_TARGET/release/$NAME-armv7"
+  sha512sum "target/$ARMV7_TARGET/release/$NAME-armv7" >"target/$ARMV7_TARGET/release/$NAME-armv7.sha512"
 else
   echo "ARMv7 artifact build: FAILED"
 fi
@@ -41,6 +59,7 @@ echo "Building Aarch64 artifact."
 if cross build -q --release --target="$AARCH_TARGET"; then
   echo "Aarch64 artifact build: SUCCESS"
   cp "target/$AARCH_TARGET/release/$NAME" "target/$AARCH_TARGET/release/$NAME-aarch64"
+  strip "target/$AARCH_TARGET/release/$NAME-aarch64"
 else
   echo "Aarch64 artifact build: FAILED"
 fi
@@ -50,6 +69,8 @@ echo "Building OSX artifact."
 if CC=o64-clang CXX=o64-clang++ LIBZ_SYS_STATIC=1 cargo build -q --release --target="$OSX_TARGET"; then
   echo "OSX artifact build: SUCCESS"
   cp "target/$OSX_TARGET/release/$NAME" "target/$OSX_TARGET/release/$NAME-osx"
+  strip "target/$OSX_TARGET/release/$NAME-osx"
+  sha512sum "target/$OSX_TARGET/release/$NAME-osx" >"target/$OSX_TARGET/release/$NAME-osx.sha512"
 else
   echo "OSX artifact build: FAILED"
 fi
